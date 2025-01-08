685e8ebcd35f2f328f6f0605eea66bf910b27f33
Arthur Ryan <artryan@hotmail.com>
2025-01-08 11:13:25 -0300

    Prepare the Web microservice  to consome the Product microservice

---
+++ GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+
+        //GET
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something that wrong when calling the API: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        //POST
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string URL, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(URL, content);
+        }
+
+        //PUT
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string URL, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(URL, content);
+        }
+    }
+}
